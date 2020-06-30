82467305dddcc320a63c3be4871dbfdefd32fa41
vit con lon ton <>
2020-06-30 14:52:08 +0700

    Reusing code at rest_client.dart

--- lib/data/network/rest_client.dart
+++ lib/data/network/rest_client.dart
@@ -6,39 +6,31 @@ import 'package:http/http.dart' as http;
 import 'exceptions/network_exceptions.dart';
 
 class RestClient {
-
   // instantiate json decoder for json serialization
   final JsonDecoder _decoder = JsonDecoder();
 
   // Get:-----------------------------------------------------------------------
   Future<dynamic> get(String url) {
-    return http.get(url).then((http.Response response) {
-      final String res = response.body;
-      final int statusCode = response.statusCode;
-
-      if (statusCode < 200 || statusCode > 400 || json == null) {
-        throw NetworkException(
-            message: "Error fetching data from server", statusCode: statusCode);
-      }
-
-      print(res);
-      return _decoder.convert(res);
-    });
+    return http.get(url).then(_createResponse);
   }
 
   // Post:----------------------------------------------------------------------
   Future<dynamic> post(String url, {Map headers, body, encoding}) {
     return http
         .post(url, body: body, headers: headers, encoding: encoding)
-        .then((http.Response response) {
-      final String res = response.body;
-      final int statusCode = response.statusCode;
+        .then(_createResponse);
+  }
 
-      if (statusCode < 200 || statusCode > 400 || json == null) {
-        throw NetworkException(
-            message: "Error fetching data from server", statusCode: statusCode);
-      }
-      return _decoder.convert(res);
-    });
+  // Response:------------------------------------------------------------------
+  dynamic _createResponse(http.Response response) {
+    final String res = response.body;
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      throw NetworkException(
+          message: 'Error fetching data from server', statusCode: statusCode);
+    }
+
+    return _decoder.convert(res);
   }
 }
